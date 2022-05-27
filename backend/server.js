ee1e83efbb0ece5454245c755cb247f59c82395e
isVishwas <vishwasp1000@gmail.com>
2022-05-27 15:44:00 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,7 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('API is running....')
-})
+
 
 app.use('/api/users', userRoutes)
 app.use('/api/post', postRoutes)
@@ -33,6 +31,20 @@ app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
+
+
+
+if(process.env.NODE_ENV == "production"){
+  app.use(express.static(path.join(__dirname,"/client/build")))
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  })
+}else{
+    app.get('/', (req, res) => {
+      res.send('API is running....')
+    })
+}
+
 
 app.listen(
   PORT,
