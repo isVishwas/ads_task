ec3b2f92acf58ad1850aa94e24413d207b98124a
isVishwas <vishwasp1000@gmail.com>
2022-05-27 15:54:51 +0530

    make it better

---
+++ client/build/precache-manifest.9ce09b6890ad127409b810239cfa4d41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7eaca96be07c2e2c9643dad76bc0ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e27e5cd9ef0e368a444",
+    "url": "/static/css/main.14bafacb.chunk.css"
+  },
+  {
+    "revision": "58af7f579919a966714d",
+    "url": "/static/js/2.e9a961ee.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.e9a961ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e27e5cd9ef0e368a444",
+    "url": "/static/js/main.e677ea5c.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
